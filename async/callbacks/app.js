152410c819b20a9969be8136261dbe66bfefd4f3
Emerson Lopes <emersonlbr@icloud.com>
2018-02-03 19:18:02 -0200

    callbacks

---
+++ async/callbacks/app.js
@@ -0,0 +1,27 @@
+console.log('hello world');
+
+// a higher order function accepts a callback as a parameter
+
+// without a for each
+let arr = [1, 5, 3, 4, 5, 8];
+
+function double(arr) {
+  for(let i = 0; i < arr.length; i++) {
+    console.log(arr[i] * 2);
+  }
+}
+
+double(arr);
+
+function double1(arr) {
+  for(let i in arr) {
+    console.log(i * 2 + 'yey');
+  }
+}
+
+double1(arr);
+
+
+arr.forEach(element => {
+  console.log(element * 2);
+});
