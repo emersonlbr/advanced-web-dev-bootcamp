2e8aca7fd64ab95c3d896750874f383574659fe7
Emerson Lopes <emersonlbr@icloud.com>
2018-02-10 15:05:21 -0200

    intro to promises

---
+++ promises/app.js
@@ -0,0 +1,10 @@
+console.log('hello');
+
+
+// javascript is linear, it can't run 2 things at the same time
+
+// promises 
+// a promise is an object that represents a task that will be completed in the future
+
+// didnt understand shit about promises .then .catch
+
