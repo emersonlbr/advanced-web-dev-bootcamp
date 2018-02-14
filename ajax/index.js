6e061ad5b79c3de74e80d90e24147fbdba1835ca
Emerson Lopes <emersonlbr@icloud.com>
2018-02-13 22:01:30 -0200

    fetch

--- ajax/index.js
+++ ajax/index.js
@@ -2,21 +2,19 @@ console.log('hello world');
 // ajax is just an approach
 // xml and json
 // apis exists to send data between computers 
+
+
 // XMLHttpRequest - no one uses it anymore 
 
-// btn.addEventListenner('click', () => {
-//   //do stuff here
-//   console.log("Hello World!");
-// });
 
-// let dq = 'document.querySelector';
+// FETCH API Exemple
+let url = 'https://randomuser.me/api';
+fetch(url)
+.then(function(res) {
+  console.log(res);
+})
+.catch(function(error) {
+  console.log(error);
+});
 
-function dq(selector){return document.querySelector(selector);}
-let h1 = dq('h1');
-dq('h1').onclick = () => {console.log('yey');};
 
-// work like hell, do everything you can to accomplish the goal
-// I want to make the world a better place
-// I want to matter
-
-// don't get afraid of new arenas 
