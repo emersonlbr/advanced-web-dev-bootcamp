e410048018dc75886091ad3159825e59f390f8fb
Emerson Lopes <emersonlbr@icloud.com>
2018-02-18 16:41:47 -0300

    add uptade route

--- nodeapi/todoapis/routes/todos.js
+++ nodeapi/todoapis/routes/todos.js
@@ -6,6 +6,7 @@ const db = require('../models');
 // all routes start with /api/todos
 // using promises instead of callbacks
 
+// show all route
 router.get('/', (req, res)=> {
   db.Todo.find()
   .then((todos) => {
@@ -16,6 +17,7 @@ router.get('/', (req, res)=> {
   });
 });
 
+// create route
 router.post('/', (req, res) => {
   db.Todo.create(req.body)
   .then((newTodo) => {
@@ -23,6 +25,32 @@ router.post('/', (req, res) => {
     res.status(201).json(newTodo);
   })
   .then((err) => {
+    res.send(err);
+  });
+});
+
+
+// show id route
+router.get('/:todoId', (req, res) => {
+  db.Todo.findById(req.params.todoId)
+  .then((foundTodo) => {
+    res.json(foundTodo);
+  })
+  .then((err) => {
+    res.send(err);
+  });
+});
+
+// update route
+router.put('/:todoId', (req, res) => {
+  // req.body is what we are going to update
+  // new true will give you the updated version
+  // by default it returns the old version
+  db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true})
+  .then((todo) => {
+    res.json(todo);
+  })
+  .catch((err) => {
     res.send(err);
   });
 });
