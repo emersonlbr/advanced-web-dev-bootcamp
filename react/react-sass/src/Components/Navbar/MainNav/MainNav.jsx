9e1d411a62fc1147a4094cd24a3bd17ea6d38def
lupuselit <emersonlbr@icloud.com>
2018-03-17 20:02:40 -0300

    add react-sass

---
+++ react/react-sass/src/Components/Navbar/MainNav/MainNav.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class MainNav extends Component {
+
+  render() {
+    return (
+      <div className="MainNav">
+        <a href="https://lupuselit.github.io">Emerson Lopes</a>
+
+        <span><i className="ion-ios-barcode-outline"></i></span>
+      </div>
+    );
+  }
+}
+
+export default MainNav;
