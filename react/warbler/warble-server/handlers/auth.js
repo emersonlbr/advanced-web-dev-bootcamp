9178e66a7f3de9e80ac542bf4572017da3d6c7c5
lupuselit <emersonlbr@icloud.com>
2018-03-25 13:18:47 -0300

    add auth

---
+++ react/warbler/warble-server/handlers/auth.js
@@ -0,0 +1,21 @@
+const db = require("../models");
+// to mark uses as logged in
+const jwt = require("jsonwebtoken");
+
+
+exports.signup = async function(req, res, next) {
+  try {
+    // create a user
+
+    // create a token (signing a token)
+      // process.env.SECRET_KEY
+  } catch (error) {
+    // see what kind of error
+
+    // if it is a certain error
+
+    // respond with username/email already taken
+
+    // otherwise just send back a generic 400
+  }
+}
